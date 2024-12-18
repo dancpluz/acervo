9dd851276169f0e02f2798ad214f28d4ea9705c8
dancpluz <dan08jan@gmail.com>
2024-12-18 17:13:19 -0300

    fix: calculator minor fix

--- components/SelectableChips.tsx
+++ components/SelectableChips.tsx
@@ -5,7 +5,7 @@ export default function SelectableChips<Chip>({ chips, chipText, chipNumber, pla
   return (
     <div className='flex w-full flex-wrap gap-2'>
       {chips.length > 0 ? chips.map((chip) =>
-        <div key={chip[chipText]} onClick={() => { selectedChip && chip.ref === selectedChip.ref ? setFunction(undefined) : setFunction(chip) }} className={`flex border transition-colors rounded-lg cursor-pointer h-10 border-secondary gap-1 items-center px-2 ${chip === selectedChip ? 'bg-primary border-primary text-background [&>div]:text-foreground [&>div]:bg-background' : 'hover:bg-secondary/20'}`}>
+        <div key={chip[chipText]} onClick={() => { selectedChip === chip && chip.ref === selectedChip.ref ? setFunction(undefined) : setFunction(chip) }} className={`flex border transition-colors rounded-lg cursor-pointer h-10 border-secondary gap-1 items-center px-2 ${chip === selectedChip ? 'bg-primary border-primary text-background [&>div]:text-foreground [&>div]:bg-background' : 'hover:bg-secondary/20'}`}>
           {chip[chipText]}
           <div className='bg-secondary transition-colors rounded-sm px-1 text-sm'>
             {chip[chipNumber] === '' ? 'Grátis' : formatPercent(chip[chipNumber] as number)+'%'}
